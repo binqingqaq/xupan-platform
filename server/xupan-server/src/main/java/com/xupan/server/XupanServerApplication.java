5c3e0bc3f533edbaebb09465b750d56fb7f8830b
binqingqaq <3519958191@qq.com>
2026-09-08 15:30:34 +0800

    chore: initialize xupan platform project

---
+++ server/xupan-server/src/main/java/com/xupan/server/XupanServerApplication.java
@@ -0,0 +1,13 @@
+package com.xupan.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XupanServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XupanServerApplication.class, args);
+    }
+
+}
